0dcd825d98cab303e73d3b4f3da8c17558608692
Teemu Jormanainen <jormanainen.teemu@gmail.com>
2025-08-20 09:29:00 +0300

    Created add and remove methods

--- Book.cs
+++ Book.cs
@@ -2,7 +2,7 @@ namespace Book;
 
 public class Book
 {
-    List<Book> Booklist;
+    public static List<Book> Booklist = new List<Book>();
     public string Title { get; set; }
     public string Author { get; set; }
     public string Genre { get; set; }
@@ -13,7 +13,7 @@ public class Book
 
     public Book(string title, string author, string genre, int year)
     {
-        this.Booklist = new List<Book>();
+        
         this.Title = title;
         this.Author = author;
         this.Genre = genre;
@@ -23,11 +23,66 @@ public class Book
 
     public void AddBook()
     {
+        Console.WriteLine("Lisää kirja antamalla seuraavat tiedot");
+
+        Console.WriteLine("Anna kirjan nimi");
+        string? title = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Anna kirjan tekijän nimi");
+        string? author = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Anna kirjan genre");
+        string? genre = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Anna kirjan julkaisuvuosi");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        Book newBook = new Book(title, author, genre, year);
+
+        bool found = false;
+
+        foreach (Book addBook in Booklist)
+        {
+            if (addBook.Title.Equals(newBook.Title, StringComparison.CurrentCultureIgnoreCase) && addBook.Author.Equals(newBook.Author, StringComparison.CurrentCultureIgnoreCase))
+            {
+                found = true;
+                break;
+            }
+
+        }
+        if (found)
+            {
+                Console.WriteLine($"{title}: {author} can already be found on the book list");
+            }
+        else
+            {
+                Console.WriteLine($"{title}: {author} is added to the book list");
+                Booklist.Add(newBook);
+            }
+
 
     }
 
     public void RemoveBook()
     {
+        Console.WriteLine("Anna seuraavat poistettavan kirjan tiedot");
+
+        Console.WriteLine("Anna kirjan nimi");
+
+        string? title = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Anna kirjan tekijä");
+
+        string? author = Console.ReadLine() ?? "";
+
+        for (int i = Booklist.Count - 1; i >= 0; i--)
+        {
+            if (Booklist[i].Title.Equals(title, StringComparison.CurrentCultureIgnoreCase) && Booklist[i].Author.Equals(author, StringComparison.CurrentCultureIgnoreCase))
+            {
+                Booklist.RemoveAt(i);
+                Console.WriteLine($"{title}: {author} has been deleted from the book list");
+            }
+        }
 
     }
 
